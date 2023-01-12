9a646081bd92e9d59270805f7328468fee6e234b
Cristina Reyes <cristina.reyes@destacados.cl>
2023-01-11 21:07:49 -0400

    ultimos cambias

--- kiosku/src/app/app.module.ts
+++ kiosku/src/app/app.module.ts
@@ -5,11 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductosComponent } from './productos/productos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { EditarProductoComponent } from './editar-producto/editar-producto.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { RestablecerContrasenaComponent } from './restablecer-contrasena/restablecer-contrasena.component';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductosComponent
+    ProductosComponent,
+    HeaderComponent,
+    EditarProductoComponent,
+    LoginComponent,
+    RegistroComponent,
+    RestablecerContrasenaComponent
   ],
   imports: [
     BrowserModule,
